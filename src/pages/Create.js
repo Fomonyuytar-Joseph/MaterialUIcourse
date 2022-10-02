b43048b4e922478e95f8b7e2c384a787a4fc6c7f
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-10-02 20:38:48 +0100

    add Button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,37 @@
-import React from 'react'
-
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { Button } from "@material-ui/core";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import {Container} from "@material-ui/core";
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography variant="h6">Create a New Note</Typography>
+
+
+<Button
+onClick={()=> console.log("you clicked me")}
+type="submit"
+variant="contained"
+color="secondary"
+
+
+>
+Submit
+</Button>
+
+
+
+
+{/* 
+      <Button type="submit">submit</Button>
+      <Button variant="outlined" type="submit" color="secondary">submit</Button>
+   <ButtonGroup variant="contained" color="secondary">
+    <Button>One</Button>
+    <Button>Two</Button>
+    <Button>Three </Button>
+   </ButtonGroup> */}
+
+    </Container>
+  );
 }
