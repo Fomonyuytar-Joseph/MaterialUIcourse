fa8a9841aef70f6a6887d1413fc6fc9168a3ad41
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-10-05 08:55:04 +0100

    add checkbox component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,46 +1,49 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import{Button} from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
-import {Container} from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import { makeStyles } from "@material-ui/core";
-import {TextField }from "@material-ui/core";
+import { TextField } from "@material-ui/core";
 import { useState } from "react";
-import {Radio} from "@material-ui/core";
-import {RadioGroup} from "@material-ui/core";
+import { Radio } from "@material-ui/core";
+import { RadioGroup } from "@material-ui/core";
+import { FormControlLabel } from "@material-ui/core";
+import { FormLabel } from "@material-ui/core";
+import { FormControl } from "@material-ui/core";
 
 const useStyles = makeStyles({
-  field:{
-    marginTop:20,
-    marginBottom:20,
-    display:'block'
-  }
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
 });
 export default function Create() {
   const classes = useStyles();
-  const [title, setTitle] = useState('')
-  const [details, setDetails] = useState('')
-  const [titleError, setTitleError] = useState(false)
-  const [detailsError, setDetailsError] = useState(false)
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [category, setCategory] = useState("");
 
-  const handleSubmit =(e)=>{
-        e.preventDefault()
-        setTitleError(false)
-        setDetailsError(false)
-        if(title==''){
-          setTitleError(true)
-        }
-        if(details==''){
-          setDetailsError(true)
-        }
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
 
-        if(title && details ){
-          console.log(title , details)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+    if (title == "") {
+      setTitleError(true);
+    }
+    if (details == "") {
+      setDetailsError(true);
+    }
 
-        }
-       
-  }
+    if (title && details) {
+      console.log(title, details, category);
+    }
+  };
   return (
     <Container>
       <Typography
@@ -53,47 +56,49 @@ export default function Create() {
       </Typography>
 
       <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-        <TextField  
-        onChange={(e)=> setTitle(e.target.value)}
-        label="Note Label" 
-        variant="outlined"
-        color="secondary"
-        className={classes.field}
-        fullWidth
-        required
-        error={titleError}
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          label="Note Label"
+          variant="outlined"
+          color="secondary"
+          className={classes.field}
+          fullWidth
+          required
+          error={titleError}
         />
-      
-        <TextField 
-        onChange={(e)=> setDetails(e.target.value)} 
-        label="Details" 
-        variant="outlined"
-        color="secondary"
-        className={classes.field}
-        multiline
-        rows={4}
-        fullWidth
-        required
-        error={detailsError}
-        />
-        <RadioGroup>
-        <Radio value="hello" />
-        <Radio value="goodbye" />
-        </RadioGroup>
-       
-       <Button
-        
-        type="submit"
-        variant="contained"
-        color="secondary"
-        // startIcon={<SendIcon/>}
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-        Submit
-      </Button>
-      </form>
 
-     
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          className={classes.field}
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+            <FormControlLabel value="money" control={<Radio />} label="money" />
+            <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+            <FormControlLabel value="reminders" control={<Radio />}label="Reminders"/>
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          // startIcon={<SendIcon/>}
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
 
       {/* icons */}
       {/* <br/>
