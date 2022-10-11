6ea20c6222d49e29b4e0cbba3f163d66d95a8f35
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-10-11 17:00:55 +0100

    add Appdrawer and Avatar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { makeStyles } from "@material-ui/core";
+import {  makeStyles } from "@material-ui/core";
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
@@ -12,32 +12,49 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText'
 import { AddCircleOutlined, SubjectOutlined } from '@material-ui/icons';
 import { useHistory, useLocation } from 'react-router-dom';
+import {format} from "date-fns";
+import {Avatar} from '@material-ui/core';
+
 
      const drawerWidth= 240
-const useStyles = makeStyles({
-              page:{
-                background:"#f9f9f9",
-                width:"100%"
-
-              },
-              drawer:{
-                width:drawerWidth,
-                
-
-              },
-              drawerPaper:{
-                width:drawerWidth,
-                
-
-              },
-              root:{
-                  display:"flex"
-              },
-              active:{
-                background:"#f4f4f4"
-              }
-            
-})
+     const useStyles = makeStyles((theme) => {
+      return {
+        page: {
+          background: '#f9f9f9',
+          width: '100%',
+          padding: theme.spacing(3),
+        },
+        root: {
+          display: 'flex',
+        },
+        drawer: {
+          width: drawerWidth,
+        },
+        drawerPaper: {
+          width: drawerWidth,
+        },
+        active: {
+          background: '#f4f4f4'
+        },
+        title: {
+          padding: theme.spacing(2),
+        },
+        appBar:{
+          width: `calc(100% - ${drawerWidth}px)`,
+          marginLeft: drawerWidth,
+        },
+        toolbar:theme.mixins.toolbar,
+        date:{
+          flexGrow:1
+        },
+        avatar:{
+          marginLeft: theme.spacing(2 )
+        }
+           
+        
+      }
+    })
+    
 
 function Layout({children}) {
     const classes = useStyles()
@@ -59,6 +76,20 @@ function Layout({children}) {
   return (
     <div className={classes.root}>
     {/* app bar */}
+    <AppBar
+     className={classes.appBar}
+     elevation={0}>
+      <Toolbar  >
+        <Typography className={classes.date}>
+       Today is the  {format(new Date(), 'do MMMM Y')}
+        </Typography>
+        <Typography>
+          Joseph
+        </Typography>
+        <Avatar src="/mario-av.png" className={classes.avatar}/> 
+      </Toolbar>
+
+    </AppBar>
 
 
 
@@ -71,13 +102,13 @@ function Layout({children}) {
         classes={{paper:classes.drawerPaper}}
 
         >
-      <Typography variant='h5'>
-        Joseph Notes
+      <Typography variant='h5' className={classes.title}>
+      
       </Typography>
 
       {/* List Links */}
       <List>
-          {
+          { 
             menuItems.map(item =>(
               <ListItem
               button
@@ -98,7 +129,10 @@ function Layout({children}) {
     </div>
 
         <div className={classes.page}>
-        {children}
+          <div className={classes.toolbar }> </div>
+          {children}
+           
+        
         </div>
     
     
